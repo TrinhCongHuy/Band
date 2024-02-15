725e95b9acf278a39985cddfd27602260ea835ab
TrinhCongHuy <trinhconghuy.2003@gmail.com>
2024-02-15 23:22:42 +0700

    update nav

--- band.js
+++ band.js
@@ -1,3 +1,34 @@
+// Nav
+const header = document.getElementById('header');
+const mobileMenu = document.getElementById('mobile-menu');
+const headerHeight = header.clientHeight;
+mobileMenu.onclick = function() {
+    const isClose = header.clientHeight === headerHeight;
+
+    if (isClose) {
+        header.style.height = 'auto';
+    } else{
+        header.style.height = null; 
+    }
+}
+// Tự động đóng menu
+const menuItems = document.querySelectorAll('#nav li a[href*="#"]')
+for (let i = 0 ; i < menuItems.length; i++){
+
+    const menuItem = menuItems[i];
+    menuItem.onclick = function(event){
+    const isParentMenu = this.nextElementSibling && menuItem.nextElementSibling.classList.contains('subnav');
+        if (!isParentMenu){
+            header.style.height = null;
+        }else{
+            event.prevenDefault();
+        }
+        
+    }
+}
+
+
+// Modal
 const buyBtns= document.querySelectorAll('.js-sticket')
 const modal = document.querySelector('.js-modal')
 const modalContainer = document.querySelector('.js-modal-container')
@@ -19,4 +50,8 @@ modalClose.addEventListener('click',hideBuyTickets)
 modal.addEventListener('click',hideBuyTickets)
 modalContainer.addEventListener('click',function(event){
     event.stopPropagation()
+})
+
+modalClose.addEventListener("click", () => {
+    console.log("oke")
 })
